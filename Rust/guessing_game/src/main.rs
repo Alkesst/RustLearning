b8015695e54c02aef0b4c1acfb1cd5684f81c51a
Alkesst <alec.augal@gmail.com>
2018-04-29 10:24:37 +0200

    Finichsed guessing game tutorial

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,12 +1,30 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
+    loop{
+        println!("Please input your guess.");
+        let mut guess = String::new(); // La cláusula String::new() es el método estático ::new() de la clase String. 
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {}", guess);
 
-    let mut guess = String::new(); // La cláusula String::new() es el método estático ::new() de la clase String. 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number){
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("To big!"),
+            Ordering::Equal     => {println!("You win!"); break;}
+        }
+    }
 }
